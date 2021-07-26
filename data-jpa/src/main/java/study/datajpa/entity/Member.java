b12cc9be3e694e36372807f13c16ed30ab969e84
janghwan_bae <janghwan_bae@tmax.co.kr>
2021-07-26 18:33:29 +0900

    스프링 data jpa chapter 2 완료

--- data-jpa/src/main/java/study/datajpa/entity/Member.java
+++ data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -1,15 +1,13 @@
 package study.datajpa.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username","age"}) // 연관관계는 ToString 하지 말것
 public class Member {
 
     @Id @GeneratedValue
@@ -18,12 +16,32 @@ public class Member {
 
     private String username;
 
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
 
     public Member(String username) {
         this.username = username;
     }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null){
+            changeTeam(team);
+        }
+
+    }
+
     //jpa가 프록시 할때 private면 생성시 문제가 생길 수 있어서 protected로 하는게 좋음
-    //따로 생성자 선언시 꼭 선언해줘야함
-    protected Member(){
+    //따로 생성자 선언시 꼭 선언해줘야함 또는 @NoArgsConstructor(access = AccessLevel.PROTECTED) 사용
+//    protected Member(){
+//    }
+
+    public void changeTeam(Team team){
+        this.team = team;
+        team.getMembers().add(this);
     }
 }
